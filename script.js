d5342675aa6fd9414c7f24c81be4ca50c09c3b9d
Gabriel Valencia <gabrielvalencia@Gabriels-MacBook-Pro-3.local>
2022-08-12 08:27:58 -0500

    final bonus mission complete

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
         response.json().then(function (astronauts) {
             console.log(astronauts);
-            astronauts = mergeSort(astronauts);
+            document.getElementById('titleWithNumber').innerHTML += ` (${astronauts.length})`;
+            astronauts.sort((a, b) => {
+                return b.hoursInSpace - a.hoursInSpace;
+                });
+
+            //astronauts = mergeSort(astronauts);
             const div = document.getElementById("container");
             let i = 2;
             for (i = 0; i < astronauts.length; i++) {
@@ -15,7 +20,7 @@ window.addEventListener("load", function () {
       <ul>
          <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
          <li>Active: <span class="${astronauts[i].active ? "active" : "inactive"}">${astronauts[i].active}</span>
-         <li>Skills: ${astronauts[i].skills}</li>
+         <li>Skills: ${astronauts[i].skills.join(", ")}</li>
       </ul>
    </div>
    <img class="avatar" src="${astronauts[i].picture}">
@@ -30,41 +35,11 @@ window.addEventListener("load", function () {
     <ol>
         <li>Display the astronauts sorted from most to least time in space. ✅</li>
         <li>Make the "Active: true" text green, for astronauts that are still active (active is true). ✅</li>
-        <li>Add a count of astronauts to the page.</li>
+        <li>Add a count of astronauts to the page. ✅</li>
     </ol>
 </div>
 `;
 
         });
     });
-});
-
-function merge(left, right) {
-    let arr = [];
-    while (left.length && right.length) {
-        if (left[0].hoursInSpace > right[0].hoursInSpace) {
-            arr.push(left.shift());
-        } else {
-            arr.push(right.shift());
-        }
-    }
-
-    return [...arr, ...left, ...right];
-}
-
-function mergeSort(array) {
-    const half = array.length / 2;
-
-    if (array.length < 2) {
-        return array;
-    }
-
-    const left = array.splice(0, half);
-    return merge(mergeSort(left), mergeSort(array));
-}
-
-// 
-
-// 1. 
-// 2. 
-// 3. 
+}); 
